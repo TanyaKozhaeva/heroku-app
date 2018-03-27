58b5d95d172f907f1390baa1cbc2494ed1e16eba
TanyaKozhaeva <tanyakozhaeva@yandex.ua>
2018-03-27 20:14:51 +0300

    comm

--- src/app/directives/password-validation.directive.ts
+++ src/app/directives/password-validation.directive.ts
@@ -10,9 +10,10 @@ export class PasswordValidationDirective implements Validator {
 
   validate(c: FormControl): ValidationErrors {
     const hasNumber = /[0-9]/.test(c.value);
-    const hasCapitalLetter = /[A-Z]/.test(c.value);
+    //const hasCapitalLetter = /[A-Z]/.test(c.value);
     const hasLowercaseLetter = /[a-z]/.test(c.value);
-    const passwordValid = hasNumber && hasCapitalLetter && hasLowercaseLetter;
+    //const passwordValid = hasNumber && hasCapitalLetter && hasLowercaseLetter;
+    const passwordValid = hasNumber && hasLowercaseLetter;
  
     const message = {
       'passwordValidation': {
